b7a6a28981a7a84dc50ecb606dc1e40ab73b3f25
faizSource <90572388+faizSource@users.noreply.github.com>
2021-09-20 00:30:00 +0530

    Adding the python file for json to graph

--- IP-Mac-Haj1.py
+++ IP-Mac-Haj1.py
@@ -1,24 +1,23 @@
 import os
-import sys
 import time
 import threading
 import json
-import networkx as nx
 import xmltodict
-from matplotlib import pyplot as plt
-
+import networkx as nx
+import matplotlib.pyplot as plt
+import sys
 
 def cmdcmd():
-    os.popen("nmap -sP -PI -PT -oX nmap_out.xml 192.168.1.1/24")
+    os.popen("nmap -sP -PI -PT -oX /tmp/nmap_out.xml 10.94.208.4")
 
 def xmltojson():
-    with open("nmap_out.xml") as xml_file:
+    with open("/tmp/nmap_out.xml") as xml_file:
         data_dict = xmltodict.parse(xml_file.read())
 
     xml_file.close()
     json_data = json.dumps(data_dict)
 
-    with open("nmap_data.json", "w") as json_file:
+    with open("/tmp/nmap_data.json", "w") as json_file:
         json_file.write(json_data)
 
     json_file.close()
@@ -28,15 +27,24 @@ def make_graph():
         json_data = json.loads(f.read())
 
     G = nx.DiGraph()
+    print(json_data['nmaprun']['host'])
+    hosts = [elem['address']['@addr'] for elem in json_data['nmaprun']['host']]
+    print(hosts)
     G.add_nodes_from(
-       [elem['address']['@addr']
+        [elem['address']['@addr']
          for elem in json_data['nmaprun']['host']]
     )
-    #G.add_edges_from(
-     #   (elem['address']['@addr'], elem['host'])
-      #  for elem in json_data['nmaprun']['host']
-     #)
+    p = []
+    for count, ip_add in enumerate(hosts):
+        if count != len(hosts) - 1:
+            p.append((ip_add, hosts[count + 1]))
+            print(p)
+    G.add_edges_from(p)
 
+    #G.add_edges_from(
+    #    (elem['from']['status'], elem['status'])
+    #    for elem in json_data['host']
+    #)
     nx.draw(
         G,
         with_labels=True
@@ -44,20 +52,21 @@ def make_graph():
     plt.savefig("testx1.png")
     plt.waitforbuttonpress()
 
-#make_graph()
-#sys.exit()
+make_graph()
+sys.exit()
 
 while True:
-    thread1 = threading.Thread(target=cmdcmd)
-    thread1.start()
-    print("Scanned IPs...")
-    thread1.join()
-    thread2 = threading.Thread(target=xmltojson)
-    thread2.start()
-    thread2.join()
-    print("Converted to json...")
-    time.sleep(15)
+    #thread1 = threading.Thread(target=cmdcmd)
+    #thread1.start()
+    #print("Scanned IPs...")
+    #thread1.join()
+    #thread2 = threading.Thread(target=xmltojson)
+    #thread2.start()
+    #thread2.join()
+    #print("Converted to json...")
+    #time.sleep(15)
     thread3 = threading.Thread(target=make_graph)
     thread3.start()
     thread3.join()
-    print("made graph..")
+    print("Converted to json...")
+
